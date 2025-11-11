0c0fa9144d0e016e0bcc0e56520635e76ac18d32
dev <dev@mail>
2025-11-11 20:16:39 +0530

    nodemailer is removed because render is paid

--- backend/utilities/email.js
+++ backend/utilities/email.js
@@ -1,16 +1,5 @@
-const nodemailer = require('nodemailer')
-const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-        user: process.env.MAIL,
-        pass: process.env.MAIL_PASSWORD
-    }
-})
+
 const sendEmail = async (message) => {
-    const info = await transporter.sendMail(message)
-    console.log(info)
-    return info.response;
+  return true
 }
 module.exports = sendEmail
