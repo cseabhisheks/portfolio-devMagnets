05fedbd752844fb84785b92e6228731ccc00ced4
dev <dev@mail>
2025-11-11 20:01:42 +0530

    host

--- backend/utilities/email.js
+++ backend/utilities/email.js
@@ -1,20 +1,16 @@
-// mail code
 const nodemailer = require('nodemailer')
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.MAIL,
         pass: process.env.MAIL_PASSWORD
     }
 })
 const sendEmail = async (message) => {
-
-    console.log(process.env.MAIL)
-    console.log(process.env.MAIL_PASSWORD)
     const info = await transporter.sendMail(message)
     console.log(info)
     return info.response;
 }
 module.exports = sendEmail
-
